a049f4f4d5a28ecd82281546438279dd07b98b71
davinpalmtreenft <davinh@palmtreenft.com>
2023-01-03 20:25:55 -0500

    modified gitignore to include package-lock and added back staking button to header

--- components/Header/HeaderLinks.js
+++ components/Header/HeaderLinks.js
@@ -83,6 +83,16 @@ export default function HeaderLinks(props) {
         </Link>
       </ListItem>
       <ListItem className={classes.listItem}>
+        <Link href="/staking" as="/staking">
+          <Button
+            color="transparent"
+            className={classes.navLink}
+          >
+            <Icon className={classes.icons}>currency_exchange</Icon> Staking
+          </Button>
+        </Link>
+      </ListItem>
+      <ListItem className={classes.listItem}>
         <ConnectButton
           tooltipClasses={{ tooltip: classes.tooltip }}
           navLinkClasses={classes.connectButton}
