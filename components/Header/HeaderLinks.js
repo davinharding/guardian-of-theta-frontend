72c54da5ed69a8a89baabfdc3d0856c89ff82a92
davinpalmtreenft <davinh@palmtreenft.com>
2023-01-05 19:34:04 -0500

    commenting out staked addresses in headerLinks

--- components/Header/HeaderLinks.js
+++ components/Header/HeaderLinks.js
@@ -25,12 +25,12 @@ export default function HeaderLinks(props) {
     "0x4c7d0a83d59bd47219cd5ca980047d38de07686c", // Dreamland
     "0xf20687fc0a0c6e6bb20cfb7334bc2bac20ff57c0", // Beam My Line
     "0x2b1dc7c56d17702a53a8adbc158b073b60dd9be1", // gimme the tfuel
-    "0x67fc8c72707f17761ced1e71ee9a92be36179eac", // Staked Guardian
-    "0x76d39587003800215059070dc1e36d5e939da0ac", // Staked Down With Me
-    "0xace401567d517af35c1f8e234975f95b3760a1e3", // Staked Dreamland
-    "0x14daeae94ac3e065c07d2fd1b440919f3dbeeb3e", // Staked Fly N High
-    "0x9beb67806cc909131328edd2daf822aa3bd4c30f", // Staked Beam My Line
-    "0x6cd2ddf245340bc2322de497bdaedd963c09c22c", // Staked Gimme The TFuel
+    // "0x67fc8c72707f17761ced1e71ee9a92be36179eac", // Staked Guardian
+    // "0x76d39587003800215059070dc1e36d5e939da0ac", // Staked Down With Me
+    // "0xace401567d517af35c1f8e234975f95b3760a1e3", // Staked Dreamland
+    // "0x14daeae94ac3e065c07d2fd1b440919f3dbeeb3e", // Staked Fly N High
+    // "0x9beb67806cc909131328edd2daf822aa3bd4c30f", // Staked Beam My Line
+    // "0x6cd2ddf245340bc2322de497bdaedd963c09c22c", // Staked Gimme The TFuel
   ];
 
   function getNFTsForContract(contractAddresses, accountAddress) {
