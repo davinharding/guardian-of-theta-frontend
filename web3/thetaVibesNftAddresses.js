e4e8e70d4931ac95990fe01b7a48a793c40a5812
davinharding <davinlharding@gmail.com>
2023-03-03 15:13:45 -0500

    added new nft and staking addresses

--- web3/thetaVibesNftAddresses.js
+++ web3/thetaVibesNftAddresses.js
@@ -5,4 +5,7 @@ export const thetaVibesNftAddresses = [
   "0x4c7d0a83d59bd47219cd5ca980047d38de07686c", // Dreamland
   "0xf20687fc0a0c6e6bb20cfb7334bc2bac20ff57c0", // Beam My Line
   "0x2b1dc7c56d17702a53a8adbc158b073b60dd9be1", // gimme the tfuel
+  "0xc22c7bc45e8b2acec53c474462cb42ff2536cf54", // Glorious Guitar
+  "0x10c5594f8b6d43055909498ffcdb83b9ddb6a67a", // Rastaman
+  "0xf7578080ffae18c9e3ba2eccd9628946816a7dfe", // AI Samurai
 ];
