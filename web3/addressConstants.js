b601c49c6d538a44e3141b6b76fdab26f843c230
davinharding <davinlharding@gmailcom>
2023-08-22 13:20:46 -0400

    updated address and abi for ditributor

--- web3/addressConstants.js
+++ web3/addressConstants.js
@@ -2,4 +2,4 @@ export const tvibeTokenAddress = "0x14e4c61d6aa9accda3850b201077cebf464dcb31";
 
 export const GOTTokenAddress = "0xb8a9116493e191a43224e3971c7e9f23522b0883";
 
-export const GOTDistributorAddress = "0x275603a378f549dc9ea1fe971933788c4c619197";
+export const GOTDistributorAddress = "0x56c91547f695a10a773c72cf310f8194c8656b99";
