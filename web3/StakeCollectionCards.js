a5d5f485de7dc4e11e74cf33d962fdd6e5f040f2
davinharding <davinlharding@gmailcom>
2023-10-21 15:58:31 -0400

    missing file from last commit

--- web3/StakeCollectionCards.js
+++ web3/StakeCollectionCards.js
@@ -1,26 +1,11 @@
-import Card from "components/Card/Card";
-import CardBody from "components/Card/CardBody";
-import CardFooter from "components/Card/CardFooter";
-import CardHeader from "components/Card/CardHeader";
-import useSWR, { mutate } from "swr";
+import useSWR from "swr";
 import axios from "axios";
-import { makeStyles } from "@material-ui/core/styles";
-import styles from "styles/jss/nextjs-material-kit/pages/stakingPage.js";
-import { contractMetadataKey } from "./ContractMetadataKey";
-import { WithdrawButton } from "./withdrawButton";
-import { ContractButton } from "./contractButton";
-import { nftStakingAbi } from "./nftStakingAbi";
-import { ApproveDepositSection } from "./ApproveDepositSection";
-import { nftContractAbi } from "./nftContractAbi";
 import { stakedNftAddresses } from "./stakedNftAddresses";
-
-const useStyles = makeStyles(styles);
+import { StakeCollectionCard } from "./StakeCollectionCard";
 
 const fetcher = (url) => axios.get(url).then((res) => res.data);
 
 const StakeCollectionCards = (props) => {
-  const classes = useStyles();
-
   let commaStr = "";
 
   stakedNftAddresses.forEach((e) => {
@@ -38,60 +23,18 @@ const StakeCollectionCards = (props) => {
     return null;
   }
 
+  // console.log(useCalculateRewards(e.contract, account, [e.token]))
+
   return (
     <span>
       {data.map((e, idx) => {
         return (
-          <span key={idx} style={{ marginTop: "3rem" }}>
-            <Card className={classes.stakingCard}>
-              <CardHeader color="primary">
-                {contractMetadataKey[e.contract].name} #{e.token}
-              </CardHeader>
-              <CardBody>
-                <img
-                  src={
-                    // below handles the Staked Lil Monsters contract which has unique images for each NFT whereas all the others use the same image
-                    e.contract === "0x2f9ce5a469ed1cc8ae353b55b40432c8c1745153"
-                      ? contractMetadataKey[e.contract].url + e.token + ".png"
-                      : contractMetadataKey[e.contract].url
-                  }
-                  height="100%"
-                  width="100%"
-                />
-              </CardBody>
-              <CardFooter className={classes.center}>
-                {props.staked ? (
-                  <>
-                    <WithdrawButton
-                      setTxnSuccessful={props.setTxnSuccessful}
-                      tokenId={e.token}
-                      nftAddress={e.contract}
-                    />
-                    <ContractButton
-                      contractAddress={e.contract}
-                      abi={nftStakingAbi}
-                      functionName={"claimRewards"}
-                      buttonTitle={"Collect"}
-                      sendParameter={[e.token]}
-                    />
-                  </>
-                ) : (
-                  <ApproveDepositSection
-                    setTxnSuccessful={props.setTxnSuccessful}
-                    contract={e.contract}
-                    abi={nftContractAbi}
-                    functionName={"setApprovalForAll"}
-                    buttonTitle={"Approve"}
-                    sendParameter={
-                      contractMetadataKey[e.contract].relatedContract
-                    }
-                    sendParameter2={true}
-                    token={e.token}
-                  />
-                )}
-              </CardFooter>
-            </Card>
-          </span>
+          <StakeCollectionCard
+            token={e}
+            idx={idx}
+            setTxnSuccessful={props.setTxnSuccessful}
+            staked={props.staked}
+          />
         );
       })}
     </span>
